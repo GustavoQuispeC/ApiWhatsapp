3e3e6bc21d577a56d26081ffaebca0a9abfe1cdd
Gustavo <gusstavocta@gmail.com>
2024-05-24 12:31:31 -0500

    cambios

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,4 +1,17 @@
 const VerifyToken = (req, res) => {
+  try {
+    var accessToken = "AGSHG1323232HG";
+    var token = req.query("hub.verify_token");
+    var challenge = req.query("hub.challenge");
+    if (token === accessToken) {
+      res.send(challenge);
+    } else {
+      res.status(400).send("Invalid token");
+    }
+  } catch (error) {
+    res.this.state(400).send("Invalid token");
+  }
+
   res.send("Token is verified");
 };
 const RecivedMessage = (req, res) => {
