7e6624223cc396f811dcacdf49efeb630def0a11
Gustavo <gusstavocta@gmail.com>
2024-05-24 12:53:32 -0500

    agregue Gustavo

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,17 +1,22 @@
 const VerifyToken = (req, res) => {
-  try {
-    var accessToken = "AGSHG1323232HG";
-    var token = req.query("hub.verify_token");
-    var challenge = req.body("hub.challenge");
-    if (token === accessToken) {
-      res.send(challenge);
-    } else {
+    try {
+      const accessToken = "AGSHG1323232HG";
+      const token = req.query["hub.verify_token"];
+      const challenge = req.query["hub.challenge"];
+
+      if (token === accessToken) {
+        res.send(challenge);
+      } else {
+        res.status(400).send("Invalid token");
+      }
+    } catch (error) {
       res.status(400).send("Invalid token");
     }
-  } catch (error) {
-    res.this.state(400).send("Invalid token");
-  }
-};
+  };
+
+// const VerifyToken = (req, res) => {
+//   res.send("verify token");
+// };
 
 const RecivedMessage = (req, res) => {
   res.send("Message Recived");
