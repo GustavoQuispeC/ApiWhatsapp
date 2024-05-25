e51b83d549211744a0c7b57dc9c66a48bad1cd25
Gustavo <gusstavocta@gmail.com>
2024-05-25 12:28:07 -0500

    implementacion de verifyToken and  recivedMessage

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,25 +1,36 @@
 const VerifyToken = (req, res) => {
-    try {
-      const accessToken = "AGSHG1323232HG";
-      const token = req.query["hub.verify_token"];
-      const challenge = req.query["hub.challenge"];
+  try {
+    const accessToken = "AGSHG1323232HG";
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
 
-      if (token === accessToken) {
-        res.send(challenge);
-      } else {
-        res.status(400).send("Invalid token");
-      }
-    } catch (error) {
+    if (token === accessToken) {
+      res.send(challenge);
+    } else {
       res.status(400).send("Invalid token");
     }
-  };
+  } catch (error) {
+    res.status(400).send("Invalid token");
+  }
+};
 
-// const VerifyToken = (req, res) => {
-//   res.send("verify token");
-// };
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
 
 const RecivedMessage = (req, res) => {
-  res.send("Message Recived");
+  try {
+    var entry = (req.body["entry"])[0];
+    var changes = (entry["changes"])[0];
+    var value = changes["value"];
+    var messageObject = value["messages"];
+    myConsole.log(messageObject);
+
+    res.send("EVENT_RECEIVED");
+  } catch (error) {
+    myConsole.log(error);
+    res.send("EVENT_RECEIVED");
+  }
 };
+
 
 module.exports = { VerifyToken, RecivedMessage };
